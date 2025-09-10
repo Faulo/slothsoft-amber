ef5bcfe5513fc95adbba3879dcb7d96b07d0e7c9
Daniel Schulz <faulolio@gmail.com>
2025-09-10 15:46:04 +0200

    fix: env

--- src/CLI/WineProcess.php
+++ src/CLI/WineProcess.php
@@ -8,36 +8,40 @@ use Symfony\Component\Process\Process;
 
 class WineProcess extends Process {
 
+    private const SERVER_PREFIX = 'WINEPREFIX';
+
     private const SERVER_DIRECTORY = 'wine';
 
     private const SERVER_TIMEOUT = 60;
 
-    private static function getDirectoryPrefix(): string {
+    private const SERVER_COMMAND = [
+        'wineserver',
+        '-p',
+        self::SERVER_TIMEOUT
+    ];
+
+    private static function getDirectoryPrefix(): array {
         $directory = ServerEnvironment::getDataDirectory() . DIRECTORY_SEPARATOR . self::SERVER_DIRECTORY;
         FileSystem::ensureDirectory($directory);
-        return 'WINEPREFIX=' . escapeshellarg($directory);
+        return [
+            self::SERVER_PREFIX => $directory
+        ];
     }
 
-    private static function startServer(): string {
-        $prefix = self::getDirectoryPrefix();
-        $server = new Process([
-            $prefix,
-            'wineserver',
-            '-p',
-            self::SERVER_TIMEOUT
-        ]);
+    private static function startServer(): array {
+        $env = self::getDirectoryPrefix();
+        $server = new Process(self::SERVER_COMMAND, null, $env);
         $server->run();
-        return $prefix;
+        return $env;
     }
 
     public function __construct(array $command) {
         if (PHP_OS_FAMILY !== 'Windows') {
-            $prefix = self::startServer();
+            $env = self::startServer();
 
-            array_unshift($command, $prefix);
             array_unshift($command, 'wine');
         }
 
-        parent::__construct($command);
+        parent::__construct($command, null, $env);
     }
 }
