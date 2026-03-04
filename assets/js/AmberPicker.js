a69e80db01e1f848aabadad39cd8ea56112086cb
Daniel Schulz (Garl) <faulolio@gmail.com>
2026-03-04 15:55:56 +0100

    feat: AmberPicker stub

--- assets/js/AmberPicker.js
+++ assets/js/AmberPicker.js
@@ -1,31 +1,28 @@
 "use strict";
 
+import Vue from "https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js";
+import ContextMenu from "https://cdn.jsdelivr.net/npm/@imengyu/vue3-context-menu@1/lib/vue3-context-menu.es.js";
+
 import Bootstrap from "/slothsoft@farah/js/Bootstrap";
 import AmberAPI from "/slothsoft@amber/js/AmberAPI";
 import { NS } from "/slothsoft@farah/js/XMLNamespaces";
 
 function bootstrap() {
-    window.AmberPicker = new AmberPicker(window.document);
+    const app = Vue.createApp();
+    app.use(ContextMenu);
+    app.mount(window.document);
+    alert(app);
 }
 
 export default class AmberPicker {
     #document;
-    #rootNode;
+    #contextMenu;
     #popupNode;
     #embeds = [];
 
     constructor(document) {
-        this.#document = document;
-        this.#rootNode = document.body ? document.body : document.documentElement;
-
-        this.#popupNode = this.#document.createElementNS(NS.XHTML, "div");
-        this.#popupNode.setAttribute("class", `amber-popup`);
-        this.#rootNode.appendChild(this.#popupNode);
-        this.#rootNode.addEventListener("click", this.closePopup.bind(this), false);
-
-        for (let node of this.#document.querySelectorAll("amber-embed[mode]")) {
-            this.#embeds.push(new AmberEmbed(this, node));
-        }
+        this.#contextMenu = createContextMenu();
+        this.#contextMenu.open();
     }
 
     closePopup() {
@@ -44,53 +41,6 @@ export default class AmberPicker {
                 this.#popupNode.setAttribute("class", `amber-popup amber-popup--visible amber-poup--${type}`);
                 this.#popupNode.appendChild(this.#document.importNode(infosetNode, true));
             });
-    }
-}
-
-class AmberEmbed {
-    #picker;
-    #node;
-    #type;
-    #modes = {
-        "popup": false,
-        "picker": false
-    };
-
-    constructor(picker, node) {
-        this.#picker = picker;
-        this.#node = node;
-
-        this.#type = this.#node.getAttribute("type");
-
-        for (let mode of this.#node.getAttribute("mode").split(" ")) {
-            this.#modes[mode] = true;
-        }
-
-        this.#node.setAttribute("aria-role", "button");
-        this.#node.setAttribute("tabindex", "0");
-
-        if (this.#modes.popup) {
-            this.#node.addEventListener("click", this.#activatePopup.bind(this), false);
-        }
-
-        if (this.#modes.picker) {
-            this.#node.addEventListener("contextmenu", this.#activatePicker.bind(this), false);
-        }
-    }
-
-    #activatePopup(eve) {
-        const id = parseInt(this.#node.getAttribute("id"));
-        if (id) {
-            eve.preventDefault();
-            eve.stopPropagation();
-            this.#picker.openPopup(this.#node.getAttribute("infoset"), this.#type, id);
-        }
-    }
-
-    #activatePicker(eve) {
-        eve.preventDefault();
-        eve.stopPropagation();
-        console.log("activating picker");
     }
 }
 
---
+++ assets/js/AmberPopup.js
@@ -0,0 +1,97 @@
+"use strict";
+
+import Bootstrap from "/slothsoft@farah/js/Bootstrap";
+import AmberAPI from "/slothsoft@amber/js/AmberAPI";
+import { NS } from "/slothsoft@farah/js/XMLNamespaces";
+
+function bootstrap() {
+    new AmberPopup(window.document);
+}
+
+export default class AmberPopup {
+    #document;
+    #rootNode;
+    #popupNode;
+    #embeds = [];
+
+    constructor(document) {
+        this.#document = document;
+        this.#rootNode = document.body ? document.body : document.documentElement;
+
+        this.#popupNode = this.#document.createElementNS(NS.XHTML, "div");
+        this.#popupNode.setAttribute("class", `amber-popup`);
+        this.#rootNode.appendChild(this.#popupNode);
+        this.#rootNode.addEventListener("click", this.closePopup.bind(this), false);
+
+        for (let node of this.#document.querySelectorAll("amber-embed[mode]")) {
+            this.#embeds.push(new AmberEmbed(this, node));
+        }
+    }
+
+    closePopup() {
+        while (this.#popupNode.hasChildNodes()) {
+            this.#popupNode.removeChild(this.#popupNode.lastChild);
+        }
+
+        this.#popupNode.setAttribute("class", `amber-popup`);
+    }
+
+    openPopup(infosetId, type, id) {
+        AmberAPI
+            .getViewElement(infosetId, type, id)
+            .then(infosetNode => {
+                this.closePopup();
+                this.#popupNode.setAttribute("class", `amber-popup amber-popup--visible amber-poup--${type}`);
+                this.#popupNode.appendChild(this.#document.importNode(infosetNode, true));
+            });
+    }
+}
+
+class AmberEmbed {
+    #picker;
+    #node;
+    #type;
+    #modes = {
+        "popup": false,
+        "picker": false
+    };
+
+    constructor(picker, node) {
+        this.#picker = picker;
+        this.#node = node;
+
+        this.#type = this.#node.getAttribute("type");
+
+        for (let mode of this.#node.getAttribute("mode").split(" ")) {
+            this.#modes[mode] = true;
+        }
+
+        this.#node.setAttribute("aria-role", "button");
+        this.#node.setAttribute("tabindex", "0");
+
+        if (this.#modes.popup) {
+            this.#node.addEventListener("click", this.#activatePopup.bind(this), false);
+        }
+
+        if (this.#modes.picker) {
+            this.#node.addEventListener("contextmenu", this.#activatePicker.bind(this), false);
+        }
+    }
+
+    #activatePopup(eve) {
+        const id = parseInt(this.#node.getAttribute("id"));
+        if (id) {
+            eve.preventDefault();
+            eve.stopPropagation();
+            this.#picker.openPopup(this.#node.getAttribute("infoset"), this.#type, id);
+        }
+    }
+
+    #activatePicker(eve) {
+        eve.preventDefault();
+        eve.stopPropagation();
+        console.log("activating picker");
+    }
+}
+
+Bootstrap.run(bootstrap);
